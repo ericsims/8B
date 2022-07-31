296dea749ca88f7c705cd7602328d5542de28256
Eric Sims <EricSimsM@gmail.com>
2022-07-31 14:02:58 -0600

    cool automated outjobs

--- Scripts/run_entire_proj.pas
+++ Scripts/run_entire_proj.pas
@@ -1,7 +1,16 @@
 Procedure GenerateEntireProj;
 Begin
+     // Backplane
+     GenerateOutputFiles('D:\Users\erics\Documents\16B\Backplane\Backplane.PrjPcb');
+     GenerateOutputFiles('D:\Users\erics\Documents\16B\Control Logic\Control Logic.PrjPcb');
+
+     // Compute
      GenerateOutputFiles('D:\Users\erics\Documents\16B\ALU_Ctrl\ALU_Ctrl.PrjPcb');
      GenerateOutputFiles('D:\Users\erics\Documents\16B\ALU_Logic\ALU_Logic.PrjPcb');
      GenerateOutputFiles('D:\Users\erics\Documents\16B\Scratch\Scratch.PrjPcb');
      GenerateOutputFiles('D:\Users\erics\Documents\16B\Register\Register.PrjPcb');
+     // Memories
+     GenerateOutputFiles('D:\Users\erics\Documents\16B\Memory_Proto\Memory_Proto.PrjPcb');
+     GenerateOutputFiles('D:\Users\erics\Documents\16B\Memory_SRAM\Memory_SRAM.PrjPcb');
+     GenerateOutputFiles('D:\Users\erics\Documents\16B\Memory_EEPROM\Memory_EEPROM.PrjPcb');
 End;
