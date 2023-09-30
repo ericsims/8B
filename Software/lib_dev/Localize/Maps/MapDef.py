6b7e9003dac031e6aac3ac0f4c39ef81e0e1c08a
Eric Sims <ericsimsm@gmail.com>
2023-09-30 15:12:50 -0600

    map work

--- Software/lib_dev/Localize/Maps/MapDef.py
+++ Software/lib_dev/Localize/Maps/MapDef.py
@@ -9,8 +9,10 @@ class MAP_OBJS(int, Enum):
     EMPTY     = 0, (1.0, 1.0, 1.0, 1.0)
     WALL      = 1, (0.0, 0.0, 0.0, 1.0)
     DOORWAY   = 2, (0.0, 0.0, 0.0, 0.05)
-    DOOR_NODE = 3, plt.cm.Set1(0)
-    NAV_NODE  = 4, plt.cm.Set1(1)
+    DOOR_NODE = 3, (1.0, 0.0, 0.0, 1.0)
+    NAV_NODE  = 4, (0.5, 0.5, 1.0, 1.0)
+    NAV_PATH  = 5, (1.0, 0.9, 0.0, 0.3)
+
     def __new__(cls, value, color=(0.0, 0.0, 0.0, 1.0)):
         obj = int.__new__(cls, value)
         obj._value_ = value
@@ -26,6 +28,7 @@ class MapDef:
         self.doorways   = []
         self.door_nodes = []
         self.nav_nodes  = []
+        self.nav_paths  = []
         self.size       = size
         self.units      = units
         self.bounded    = bounded
@@ -34,7 +37,7 @@ class MapDef:
     def load_file(self, file):
         with open(file, 'r') as map_cfg_file:
             map_cfg = yaml.safe_load(map_cfg_file)
-            print(map_cfg)
+            # print(map_cfg)
             self.size    = list(map_cfg['size'],)
             self.units   = map_cfg['units'],
             self.bounded = map_cfg['bounded']
@@ -53,15 +56,6 @@ class MapDef:
                     # y1 = doorway[1][1]
                     # mid_point = [(x0+x1)/2, (y0+y1)/2]
                     # self.door_nodes.append(mid_point)
-            if self.bounded:
-                x0 = 0
-                x1 = self.size[0]
-                y0 = 0
-                y1 = self.size[1]
-                self.walls.append([[x0,y0], [x1,y0]])
-                self.walls.append([[x1,y0], [x1,y1]])
-                self.walls.append([[x1,y1], [x0,y1]])
-                self.walls.append([[x0,y1], [x0,y0]])
 
 
     def gen_discrete_map(self, resolution=None):
@@ -79,37 +73,67 @@ class MapDef:
         # #     self._plot_point(dis_map, resolution, door_node, MAP_OBJS.DOOR_NODE.value)
         
         # nav
-        for nav_node in self.nav_nodes:
+        nav_nodes_dis = []
+        for idx, nav_node in enumerate(self.nav_nodes):
             self._plot_point(dis_map, resolution, nav_node, MAP_OBJS.NAV_NODE.value)
-
-
+            nav_nodes_dis.append([round(nav_node[0]*resolution),round(nav_node[1]*resolution)])
+        
         # walls and boundaries
         for wall in self.walls:
             self._plot_line(dis_map, resolution, wall, MAP_OBJS.WALL.value)
-        # if self.bounded:
-        #     x0 = 0
-        #     x1 = self.size[0]
-        #     y0 = 0
-        #     y1 = self.size[1]
-        #     self._plot_line(dis_map, resolution, [[x0,y0], [x1,y0]], MAP_OBJS.WALL.value)
-        #     self._plot_line(dis_map, resolution, [[x1,y0], [x1,y1]], MAP_OBJS.WALL.value)
-        #     self._plot_line(dis_map, resolution, [[x1,y1], [x0,y1]], MAP_OBJS.WALL.value)
-        #     self._plot_line(dis_map, resolution, [[x0,y1], [x0,y0]], MAP_OBJS.WALL.value)
+        if self.bounded:
+            x0 = 0
+            x1 = self.size[0]
+            y0 = 0
+            y1 = self.size[1]
+            self._plot_line(dis_map, resolution, [[x0,y0], [x1,y0]], MAP_OBJS.WALL.value)
+            self._plot_line(dis_map, resolution, [[x1,y0], [x1,y1]], MAP_OBJS.WALL.value)
+            self._plot_line(dis_map, resolution, [[x1,y1], [x0,y1]], MAP_OBJS.WALL.value)
+            self._plot_line(dis_map, resolution, [[x0,y1], [x0,y0]], MAP_OBJS.WALL.value)
+
+        for idx, nav_node in enumerate(self.nav_nodes):
+            self.nav_paths.append([])
+            for dir in [[1,0], [0,1], [-1,0], [0,-1]]:
+                xp = round(nav_node[0]*resolution)
+                yp = round(nav_node[1]*resolution)
+                # print(dir)
+                xp += dir[0]
+                yp += dir[1]
+                while 1:
+                    # print(f"x:{xp} y:{yp} x:{int(nav_node[0]*resolution)} y:{int(nav_node[1]*resolution)} ")
+                    xp += dir[0]
+                    yp += dir[1]
+                    if xp < 0 or xp > int(resolution*self.size[0]) or yp < 0 or yp > int(resolution*self.size[1]): break
+                    pt_val = dis_map[xp, yp]
+                    if (int(pt_val) == 1):
+                        break # i hit a wall before another nav node
+                    #    self._plot_point(dis_map, resolution, [xp/resolution,yp/resolution], MAP_OBJS.NAV_PATH_MISS.value)
+                    # if int(pt_val) == int(MAP_OBJS.NAV_NODE.value):
+                    if [xp,yp] in nav_nodes_dis:
+                        # print(f"x:{xp} y:{yp} v:{int(pt_val)} x:{int(nav_node[0]*resolution)} y:{int(nav_node[1]*resolution)} ")
+                        self._plot_line(dis_map, resolution, [[nav_node[0],nav_node[1]], [xp/resolution,yp/resolution]], MAP_OBJS.NAV_PATH.value)
+                        self._plot_point(dis_map, resolution, [xp/resolution,yp/resolution], MAP_OBJS.NAV_NODE.value)
+                        self._plot_point(dis_map, resolution, [nav_node[0],nav_node[1]], MAP_OBJS.NAV_NODE.value)
+                        self.nav_paths[idx].append(nav_nodes_dis.index([xp,yp]))
+                        break
+        # print(*[f"{n}: {p}\n" for n,p in enumerate(self.nav_paths)])
+
+   
 
         return dis_map
     
     def _plot_point(self, dis_map, resolution, node, value):
-        x = int(node[0]*resolution)
-        y = int(node[1]*resolution)
-        if x < 0 or x > resolution*self.size[0] or y < 0 or y > resolution*self.size[1]: return
+        x = round(node[0]*resolution)
+        y = round(node[1]*resolution)
+        if x < 0 or x > round(resolution*self.size[0]) or y < 0 or y > round(resolution*self.size[1]): return
         dis_map[x,y] = value
 
     def _plot_line(self, dis_map, resolution, edge, value):
         # Bresenham algo
-        x0 = int(edge[0][0]*resolution)
-        y0 = int(edge[0][1]*resolution)
-        x1 = int(edge[1][0]*resolution)
-        y1 = int(edge[1][1]*resolution)
+        x0 = round(edge[0][0]*resolution)
+        y0 = round(edge[0][1]*resolution)
+        x1 = round(edge[1][0]*resolution)
+        y1 = round(edge[1][1]*resolution)
 
         dx = abs(x1 - x0)
         if x0 < x1:
--- Software/lib_dev/Localize/Maps/parse_map_ex.py
+++ Software/lib_dev/Localize/Maps/parse_map_ex.py
@@ -11,4 +11,38 @@ for index, nn in enumerate(g.nav_nodes):
     plt.annotate(index, (x,y))
 
 plt.imshow(happy_mappy.T, origin='lower', cmap=COLOR_MAP)
+
+def follow_links_depth_first(g, current, visited):
+    visited[current] = 1
+    print(current)
+    for nextnode in g.nav_paths[current]:
+        if visited[nextnode] == 1: continue
+        follow_links_depth_first(g, nextnode, visited)
+
+def explore_map_depth_first(g):
+    visited = [0]*len(g.nav_paths)
+    follow_links_depth_first(g, 0, visited)
+    print(*[f"{n}: {p}, v:{visited[n]}\n" for n,p in enumerate(g.nav_paths)])
+
+# explore_map_depth_first(g)
+
+def follow_links_breadth_first(g, current, visited, next):
+    visited[current] = 1
+    print(current)
+    for nextnode in g.nav_paths[current]:
+        if visited[nextnode] == 1: continue
+        if nextnode in next: continue
+        next.append(nextnode)
+    while len(next)>0:
+        n = next.pop()
+        follow_links_breadth_first(g, n, visited, next)
+
+def explore_map_breadth_first(g):
+    visited = [0]*len(g.nav_paths)
+    next = []
+    follow_links_breadth_first(g, 0, visited, next)
+    print(*[f"{n}: {p}, v:{visited[n]}\n" for n,p in enumerate(g.nav_paths)])
+
+explore_map_breadth_first(g)
+
 plt.show()
