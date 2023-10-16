575e2323bbe298d1a4ba02bd43edcd97a72f19b0
Eric Sims <EricSimsM@gmail.com>
2023-10-16 00:42:03 -0600

    more math lut

--- Software/lib_dev/Math/revlookup.py
+++ Software/lib_dev/Math/revlookup.py
@@ -10,17 +10,19 @@ def byte_to_str(byte_):
     return f"{(byte_&0xFF):02X} "
 
 # y = func(x)
-def gen_reverse_lookup(func, min_v=0, max_v=2**16-1):
-    # print header
+def gen_reverse_lookup(file, func, min_v=0, max_v=2**16-1):
+    # print header to console
     if is_a_lambda(func):
         fname = inspect.getsource(func).rstrip()
     else:
         fname = func.__name__
     print(f"Generating reverse lookup table for '{fname}' for {min_v} to {max_v}")
 
+
+    # generate reverse lut
     result_x = []
     result_y = []
-    for x in range(min_v,max_v):
+    for x in range(min_v,max_v+1):
         y = math.floor(func(float(x)))
         if y in result_y:
             continue
@@ -30,19 +32,23 @@ def gen_reverse_lookup(func, min_v=0, max_v=2**16-1):
             # print(f" {y:>5}, {x:>5}")
     # plt.step(result_x, result_y)
     # plt.show()
-    return zip(result_y, result_x)
 
-with open('lib_dev/Math/rev_lookup_sqrt.dat', 'w') as lut_file:
-    lut_vals = gen_reverse_lookup(math.sqrt, min_v=0, max_v=2**16-1)
-    lut_len=0
-    for y,x in lut_vals:
+    # save lut to file
+    lut_len = 0
+    for y,x in zip(result_y, result_x):
         # print(f" {y:>5}, {x:>5}")
-        lut_file.write(byte_to_str(y))
         lut_file.write(byte_to_str(x>>8))
         lut_file.write(byte_to_str(x))
+        lut_file.write(byte_to_str(y))
         lut_file.write('\n')
         lut_len += 3
     print(f"lut length: {lut_len} bytes")
 
-# gen_reverse_lookup(lambda x:math.log(x), min_v=1, max_v=2**16-1)
 
+with open('lib_dev/Math/rev_lookup_sqrt.dat', 'w') as lut_file:
+    gen_reverse_lookup(lut_file, math.sqrt, min_v=0, max_v=2**16-1)
+
+with open('lib_dev/Math/rev_lookup_ln.dat', 'w') as lut_file:
+    gen_reverse_lookup(lut_file, math.log, min_v=1, max_v=2**16-1)
+
+# gen_reverse_lookup(file, lambda x:math.log(x,10), min_v=1, max_v=2**16-1)
