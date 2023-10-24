19269c069136f9705b83b909694c019758abaa90
Eric Sims <EricSimsM@gmail.com>
2023-10-23 23:11:45 -0600

    parses all paths for a node. just need to select next node to search

--- Software/tests/302_test_nav.asm
+++ Software/tests/302_test_nav.asm
@@ -51,7 +51,7 @@ dijkstra:
     ;            ...
     ;     |___visit_[SIZE_OF_ARRAYS-1]___|
 
-    .SIZE_OF_ARRAYS   = 5 ; this needs to be >= to the NUM_NODES in the map, but less than 40ish, so stack manipulation works (i think)  i could probably do this dynamically from the NUM_NODES var
+    .SIZE_OF_ARRAYS   = 20 ; this needs to be >= to the NUM_NODES in the map, but less than 40ish, so stack manipulation works (i think)  i could probably do this dynamically from the NUM_NODES var
  
 
     .n0           =  6
@@ -110,31 +110,24 @@ dijkstra:
     ;     popw hl
         
     .set_start:
-        __load_local a, .n0
-        __store_local a, .u ; init .u=.n0
+        __load_local b, .n0
+        __store_local b, .u ; init .u=.n0
         ; set dist[.n0] = 0
         loadw hl, BP
         subw hl, #({.dist_start})*-1
-        storew hl, .scratch1 ; TODO: these two could be pushw hl
-        push #0x00
-        push a
-        pushw .scratch1
-        call set_local_array
-        popw hl
-        popw hl
+        subw hl, b
+        load a, #0x00
+        store a, (hl)
     ; .check_if_we_visted_all_nodes? why is this ncessasay?
 
     .mark_node_visted:
+        ; set visit[.u] = 0x0E
+        __load_local b, .u
         loadw hl, BP
         subw hl, #({.visit_start})*-1
-        storew hl, .scratch1 ; TODO: these two could be pushw hl
-        push #0x0E
-        __load_local b, .u
-        push b
-        pushw .scratch1
-        call set_local_array
-        popw hl
-        popw hl
+        subw hl, b
+        load a, #0x0E
+        store a, (hl)
     .check_done: ; if u == n1
         __load_local a, .n1
         __load_local b, .u
@@ -156,8 +149,7 @@ dijkstra:
             subw hl, #({.next_node_p0})*-1
             subw hl, b
             load b, (hl)
-            load a, b
-            store a, .scratch1
+            __store_local b, .next_node
             sub b, #0xFF
             jmz ..next_node ; not a valid node
         ..get_dist:
@@ -165,14 +157,15 @@ dijkstra:
             push a
             call uart_print_itoa_hex
             
-            ;print u --> p
+            ; print u --> p
             storew #str_2, static_uart_print.data_pointer
             call static_uart_print
 
-            push .scratch1
+            __load_local a, .next_node
+            push a
             call uart_print_itoa_hex
 
-            call get_distance
+            call get_distance ; dist_to_next_node = sqrt((.u.x-.p.x)^2+(.u.y-.p.y)^2)
             popw hl
             ; distance
             push b
@@ -181,7 +174,56 @@ dijkstra:
             call static_uart_print
             call uart_print_itoa_hex
             call static_uart_print_newline
-            pop a
+
+            ...next_dist:
+                ; next_dist = dist[.u] + dist_to_next_node
+                __load_local b, .u
+                loadw hl, BP
+                subw hl, #({.dist_start})*-1
+                subw hl, b
+                load a, (hl)
+                pop b
+                add a, b
+                store a, .scratch1 ; next_dist in .scratch1
+
+                ; if dist[next_node] == 0xFF || next_dist < dist[next_node] ...\
+                ; start by saving dist[next_node], since its needed twice
+                __load_local b, .next_node
+                loadw hl, BP
+                subw hl, #({.dist_start})*-1
+                subw hl, b
+                load a, (hl)
+                store a, .scratch1+1 ; dist[next_node] in .scratch1+1
+
+                ; if dist[next_node] == 0xFF
+                ; TODO: do i actaully need this? isnt next_dist always < FF?
+                ; sub a, #0xFF
+                ; jmz ...do_update
+
+                ; or if next_dist < dist[next_node]
+                load a, .scratch1
+                load b, .scratch1+1
+                sub a, b
+                halt
+                jmc ...do_update
+                jmp ...done_with_dist
+
+            ...do_update:
+                ; dist[next_node] = next_dist
+                load a, .scratch1
+                __load_local b, .next_node
+                loadw hl, BP
+                subw hl, #({.dist_start})*-1
+                subw hl, b
+                store a, (hl)
+                ; prev[next_node] = .u
+                __load_local a, .u
+                __load_local b, .next_node
+                loadw hl, BP
+                subw hl, #({.prev_start})*-1
+                subw hl, b
+                store b, (hl)
+            ...done_with_dist:
             
         ..next_node:
             __load_local b, .path_index
@@ -195,7 +237,10 @@ dijkstra:
             popw hl
             popw hl
 
-    .do_something_else:
+    .check_closest_node:
+        ..init:
+        ; i = 0
+        ; smallest_distance = FF
         halt
     .done:
         ; discard local arrays
@@ -224,91 +269,91 @@ dijkstra:
 ; end dijkstra
 
 
-get_array_addr:
-    ; helper function that is used by set_local_array and get_local_array
-    ; accesses the stack of the calling function!
-    .n     = 7
-    .start = 6
-    #bank ram
-    .addr: #res 2 ; 2 bytes of address scratc to make this faster
-    #bank rom
-    ; store .start addr in .addr
-    loadw hl, BP
-    addw hl, #.start
-    load a, (hl)
-    store a, .addr
-    subw hl, #0x01
-    load a, (hl)
-    store a, .addr+1
+; get_array_addr:
+;     ; helper function that is used by set_local_array and get_local_array
+;     ; accesses the stack of the calling function!
+;     .n     = 7
+;     .start = 6
+;     #bank ram
+;     .addr: #res 2 ; 2 bytes of address scratc to make this faster
+;     #bank rom
+;     ; store .start addr in .addr
+;     loadw hl, BP
+;     addw hl, #.start
+;     load a, (hl)
+;     store a, .addr
+;     subw hl, #0x01
+;     load a, (hl)
+;     store a, .addr+1
     
-    ; compute offset address
-    __load_local b, .n
-    loadw hl, .addr
-    subw hl, b
-    storew hl, .addr
-    ret
+;     ; compute offset address
+;     __load_local b, .n
+;     loadw hl, .addr
+;     subw hl, b
+;     storew hl, .addr
+;     ret
 
-set_local_array:
-    ; sets value in local array
-    ; takes .start address, n index, and v value
-    ;      _______________
-    ; 8   |______.v_______|
-    ; 7   |______.n_______|
-    ; 6   |    .start     |
-    ; 5   |_______________|
-    ; 4   |_______?_______| RESERVED
-    ; 3   |_______?_______|    .
-    ; 2   |_______?_______|    .
-    ; 1   |_______?_______| RESERVED
+; set_local_array:
+;     ; sets value in local array
+;     ; takes .start address, n index, and v value
+;     ;      _______________
+;     ; 8   |______.v_______|
+;     ; 7   |______.n_______|
+;     ; 6   |    .start     |
+;     ; 5   |_______________|
+;     ; 4   |_______?_______| RESERVED
+;     ; 3   |_______?_______|    .
+;     ; 2   |_______?_______|    .
+;     ; 1   |_______?_______| RESERVED
 
-    .v     = 8
-    .n     = 7
-    .start = 6
+;     .v     = 8
+;     .n     = 7
+;     .start = 6
 
-    .init:
-        __prologue
-    .get_array_addr:
-        call get_array_addr
-    .set_val:
-        ; save val
-        __load_local a, .v
-        loadw hl, get_array_addr.addr
-        store a, (hl)
+;     .init:
+;         __prologue
+;     .get_array_addr:
+;         call get_array_addr
+;     .set_val:
+;         ; save val
+;         __load_local a, .v
+;         loadw hl, get_array_addr.addr
+;         store a, (hl)
 
-    .done:
-        __epilogue
-; set_local_array
+;     .done:
+;         __epilogue
+; ; set_local_array
 
-get_local_array:
-    ; gets value in local array
-    ; takes .start address, n index, and v value. store value to in
-    ;      _______________
-    ; 8   |______.v_______|
-    ; 7   |______.n_______|
-    ; 6   |    .start     |
-    ; 5   |_______________|
-    ; 4   |_______?_______| RESERVED
-    ; 3   |_______?_______|    .
-    ; 2   |_______?_______|    .
-    ; 1   |_______?_______| RESERVED
+; get_local_array:
+;     ; gets value in local array
+;     ; takes .start address, n index, and v value. store value to in
+;     ;      _______________
+;     ; 8   |______.v_______|
+;     ; 7   |______.n_______|
+;     ; 6   |    .start     |
+;     ; 5   |_______________|
+;     ; 4   |_______?_______| RESERVED
+;     ; 3   |_______?_______|    .
+;     ; 2   |_______?_______|    .
+;     ; 1   |_______?_______| RESERVED
 
-    .v     = 8
-    .n     = 7
-    .start = 6
+;     .v     = 8
+;     .n     = 7
+;     .start = 6
 
-    .init:
-        __prologue
-    .get_array_addr:
-        call get_array_addr
-    .set_val:
-        ; save val
-        loadw hl, get_array_addr.addr
-        load a, (hl)
-        __store_local a, .v
+;     .init:
+;         __prologue
+;     .get_array_addr:
+;         call get_array_addr
+;     .set_val:
+;         ; save val
+;         loadw hl, get_array_addr.addr
+;         load a, (hl)
+;         __store_local a, .v
 
-    .done:
-        __epilogue
-; set_local_array
+;     .done:
+;         __epilogue
+; ; set_local_array
 
 #include "../src/lib/map_utils.asm"
 
