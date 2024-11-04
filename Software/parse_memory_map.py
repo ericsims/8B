848a4c5e8b7fb1b0a9854d9e8e28f3c20bf25807
Eric Sims <EricSimsM@gmail.com>
2024-11-03 19:11:41 -0700

    flash based address decoder

--- Software/parse_memory_map.py
+++ Software/parse_memory_map.py
@@ -16,6 +16,27 @@ with open('memory_map.yaml', 'r') as instream:
             addr = m
             size = cfg_fie['layout'][m]['size']
             label = cfg_fie['layout'][m]['label']
+            loc = cfg_fie['layout'][m]['location']
             outstream.write(f'; {label}\n')
+            outstream.write(f'; MEM slot {loc}\n')
             outstream.write(f'{label:<16} = 0x{addr:04X}\n')
             outstream.write(f'{f"{label}_SIZE":<16} = 0x{size:04X}\n\n')
+
+
+    with open('ADDR_ROM.bin', 'wb') as outstream:
+        decode = [0]*(2**16)
+        for m in cfg_fie['layout']:
+            addr = m
+            size = cfg_fie['layout'][m]['size']
+            loc = int(cfg_fie['layout'][m]['location'])
+            label = cfg_fie['layout'][m]['label']
+
+            for x in range(addr, addr+size):
+                if decode[x]: raise Exception(f"{label} overlaps with another memory range")
+                decode[x] = loc
+
+        for x in decode:
+            outstream.write(int(x).to_bytes(1, 'big'))
+            
+        
+
